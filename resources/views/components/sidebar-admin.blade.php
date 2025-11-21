0ba1adc4cc3450aea2e7029cf6adb707b149d3c5
Zenil Zulfi <zenil.alfa@gmail.com>
2025-11-21 23:55:44 +0700

    lalala

--- resources/views/components/sidebar-admin.blade.php
+++ resources/views/components/sidebar-admin.blade.php
@@ -31,17 +31,18 @@
             </li>
 
             <li class="nav-item">
-                <a href="{{ route('admin.suratmasuk.index') }}" 
-                 class="nav-link {{ request()->routeIs('admin.suratmasuk.*') ? 'active' : '' }}">
-                    <i class="fa-solid fa-paper-plane"></i>
-                    <p>Surat Keluar</p>
-                </a>
-            </li>
-            <li class="nav-item">
                 <a href="{{ route('admin.suratkeluar.index') }}" 
                    class="nav-link {{ request()->routeIs('admin.suratkeluar') ? 'active' : '' }}">
                     <i class="fa-solid fa-inbox"></i>
                     <p>Surat Masuk</p>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="{{ route('admin.suratmasuk.index') }}" 
+                 class="nav-link {{ request()->routeIs('admin.suratmasuk.*') ? 'active' : '' }}">
+                    <i class="fa-solid fa-paper-plane"></i>
+                    <p>Surat Keluar</p>
                 </a>
             </li>
 
